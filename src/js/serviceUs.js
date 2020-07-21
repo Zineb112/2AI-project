93025eeb5f3743563a17c623949c52ef95f69a9e
Abbassihraf <hraf.abbassi@gmail.com>
2020-07-21 22:01:09 +0100

    DOM successfully applied

---
+++ src/js/serviceUs.js
@@ -0,0 +1,18 @@
+// DOM
+
+const cardCommu = document.querySelector('.servicesUs__card1')
+const cardAudio = document.querySelector('.servicesUs__card2')
+const cardEvent = document.querySelector('.servicesUs__card3')
+
+const imgComm = document.querySelector('.servicesUs__img1')
+const imgCommAlt = document.querySelector('.servicesUs__imgAlt1')
+
+const imgAudio = document.querySelector('.servicesUs__img2')
+const imgAudioAlt = document.querySelector('.servicesUs__imgAlt2')
+
+const imgEvent = document.querySelector('.servicesUs__img3')
+const imgEventAlt = document.querySelector('.servicesUs__imgAlt3')
+
+const divCommu = document.querySelector('.displayComm')
+const divAudio = document.querySelector('.displayAudio')
+const divEvent = document.querySelector('.displayEvent')
