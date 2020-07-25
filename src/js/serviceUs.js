85b62cdf43c2d67b2561dbc62e4987f45f6bb917
Abbassihraf <hraf.abbassi@gmail.com>
2020-07-25 11:43:24 +0100

    Add mixitUp library

--- src/js/serviceUs.js
+++ src/js/serviceUs.js
@@ -1,131 +1,131 @@
-// DOM
+// // DOM
 
-const AboutMore = document.querySelector('.aboutUs__right--more')
-const AboutMoreBtn = document.querySelector('.aboutUs__right--moreBtn')
+// const AboutMore = document.querySelector('.aboutUs__right--more')
+// const AboutMoreBtn = document.querySelector('.aboutUs__right--moreBtn')
 
-const PlusA = document.querySelector('.listPlusA')
-const ListA1 = document.querySelector('.maskListA1')
-const ListA2 = document.querySelector('.maskListA2')
-const ListA3 = document.querySelector('.maskListA3')
+// const PlusA = document.querySelector('.listPlusA')
+// const ListA1 = document.querySelector('.maskListA1')
+// const ListA2 = document.querySelector('.maskListA2')
+// const ListA3 = document.querySelector('.maskListA3')
 
-const PlusE = document.querySelector('.listPlusE')
-const ListE1 = document.querySelector('.maskListE2')
-const ListE2 = document.querySelector('.maskListE2')
-const ListE3 = document.querySelector('.maskListE3')
-const ListE4 = document.querySelector('.maskListE4')
-const ListE5 = document.querySelector('.maskListE5')
-const ListE6 = document.querySelector('.maskListE6')
-const ListE7 = document.querySelector('.maskListE7')
-const ListE8 = document.querySelector('.maskListE8')
-const ListE9 = document.querySelector('.maskListE9')
+// const PlusE = document.querySelector('.listPlusE')
+// const ListE1 = document.querySelector('.maskListE2')
+// const ListE2 = document.querySelector('.maskListE2')
+// const ListE3 = document.querySelector('.maskListE3')
+// const ListE4 = document.querySelector('.maskListE4')
+// const ListE5 = document.querySelector('.maskListE5')
+// const ListE6 = document.querySelector('.maskListE6')
+// const ListE7 = document.querySelector('.maskListE7')
+// const ListE8 = document.querySelector('.maskListE8')
+// const ListE9 = document.querySelector('.maskListE9')
 
 
 
-const cardCommu = document.querySelector('.servicesUs__card1')
-const cardAudio = document.querySelector('.servicesUs__card2')
-const cardEvent = document.querySelector('.servicesUs__card3')
+// const cardCommu = document.querySelector('.servicesUs__card1')
+// const cardAudio = document.querySelector('.servicesUs__card2')
+// const cardEvent = document.querySelector('.servicesUs__card3')
 
-const imgComm = document.querySelector('.servicesUs__img1')
-const imgCommAlt = document.querySelector('.servicesUs__imgAlt1')
+// const imgComm = document.querySelector('.servicesUs__img1')
+// const imgCommAlt = document.querySelector('.servicesUs__imgAlt1')
 
-const imgAudio = document.querySelector('.servicesUs__img2')
-const imgAudioAlt = document.querySelector('.servicesUs__imgAlt2')
+// const imgAudio = document.querySelector('.servicesUs__img2')
+// const imgAudioAlt = document.querySelector('.servicesUs__imgAlt2')
 
-const imgEvent = document.querySelector('.servicesUs__img3')
-const imgEventAlt = document.querySelector('.servicesUs__imgAlt3')
+// const imgEvent = document.querySelector('.servicesUs__img3')
+// const imgEventAlt = document.querySelector('.servicesUs__imgAlt3')
 
-const divCommu = document.querySelector('.displayComm')
-const divAudio = document.querySelector('.displayAudio')
-const divEvent = document.querySelector('.displayEvent')
+// const divCommu = document.querySelector('.displayComm')
+// const divAudio = document.querySelector('.displayAudio')
+// const divEvent = document.querySelector('.displayEvent')
 
-// EVENT
+// // EVENT
 
-AboutMoreBtn.addEventListener('click',MUs)
+// AboutMoreBtn.addEventListener('click',MUs)
 
-PlusA.addEventListener('click',ListAudio)
-PlusE.addEventListener('click',ListEvent)
+// PlusA.addEventListener('click',ListAudio)
+// PlusE.addEventListener('click',ListEvent)
 
-cardCommu.addEventListener('click',fCommunication);
-cardAudio.addEventListener('click',fAudio);
-cardEvent.addEventListener('click',fEvent);
+// cardCommu.addEventListener('click',fCommunication);
+// cardAudio.addEventListener('click',fAudio);
+// cardEvent.addEventListener('click',fEvent);
 
-// More aboutUs function
+// // More aboutUs function
 
-function MUs(){
-    AboutMore.style.display = 'block'
-    AboutMoreBtn.style.display = 'none'
-}
+// function MUs(){
+//     AboutMore.style.display = 'block'
+//     AboutMoreBtn.style.display = 'none'
+// }
 
-// More List functions
+// // More List functions
 
-function ListAudio(){
-    ListA1.style.display = 'block'
-    ListA2.style.display = 'block'
-    ListA3.style.display = 'block'
-    PlusA.style.display = 'none'
-}
+// function ListAudio(){
+//     ListA1.style.display = 'block'
+//     ListA2.style.display = 'block'
+//     ListA3.style.display = 'block'
+//     PlusA.style.display = 'none'
+// }
 
-function ListEvent(){
-    ListE1.style.display = 'block'
-    ListE2.style.display = 'block'
-    ListE3.style.display = 'block'
-    ListE4.style.display = 'block'
-    ListE5.style.display = 'block'
-    ListE6.style.display = 'block'
-    ListE7.style.display = 'block'
-    ListE8.style.display = 'block'
-    ListE9.style.display = 'block'
-    PlusE.style.display = 'none'
-}
+// function ListEvent(){
+//     ListE1.style.display = 'block'
+//     ListE2.style.display = 'block'
+//     ListE3.style.display = 'block'
+//     ListE4.style.display = 'block'
+//     ListE5.style.display = 'block'
+//     ListE6.style.display = 'block'
+//     ListE7.style.display = 'block'
+//     ListE8.style.display = 'block'
+//     ListE9.style.display = 'block'
+//     PlusE.style.display = 'none'
+// }
 
-// Communication function
+// // Communication function
 
-function fCommunication(){
-    divCommu.style.display = 'block'
-    divAudio.style.display ='none'
-    divEvent.style.display ='none'
-    imgComm.style.display ='block'
-    imgCommAlt.style.display ='none'
-    imgAudio.style.display ='none'
-    imgAudioAlt.style.display ='block'
-    imgEvent.style.display ='none'
-    imgEventAlt.style.display ='block'
-    cardAudio.classList.remove('cardActive')
-    cardEvent.classList.remove('cardActive')
-    cardCommu.classList.add('cardActive')
+// function fCommunication(){
+//     divCommu.style.display = 'block'
+//     divAudio.style.display ='none'
+//     divEvent.style.display ='none'
+//     imgComm.style.display ='block'
+//     imgCommAlt.style.display ='none'
+//     imgAudio.style.display ='none'
+//     imgAudioAlt.style.display ='block'
+//     imgEvent.style.display ='none'
+//     imgEventAlt.style.display ='block'
+//     cardAudio.classList.remove('cardActive')
+//     cardEvent.classList.remove('cardActive')
+//     cardCommu.classList.add('cardActive')
 
-}
+// }
 
-// Audio function
+// // Audio function
 
-function fAudio(){
-    divCommu.style.display = 'none'
-    divAudio.style.display ='block'
-    divEvent.style.display ='none'
-    imgAudio.style.display ='block'
-    imgAudioAlt.style.display ='none'
-    imgComm.style.display ='none'
-    imgCommAlt.style.display ='block'
-    imgEvent.style.display ='none'
-    imgEventAlt.style.display ='block'
-    cardAudio.classList.add('cardActive')
-    cardEvent.classList.remove('cardActive')
-    cardCommu.classList.remove('cardActive')
-}
+// function fAudio(){
+//     divCommu.style.display = 'none'
+//     divAudio.style.display ='block'
+//     divEvent.style.display ='none'
+//     imgAudio.style.display ='block'
+//     imgAudioAlt.style.display ='none'
+//     imgComm.style.display ='none'
+//     imgCommAlt.style.display ='block'
+//     imgEvent.style.display ='none'
+//     imgEventAlt.style.display ='block'
+//     cardAudio.classList.add('cardActive')
+//     cardEvent.classList.remove('cardActive')
+//     cardCommu.classList.remove('cardActive')
+// }
 
-// Event function
+// // Event function
 
-function fEvent(){
-    divCommu.style.display = 'none'
-    divAudio.style.display ='none'
-    divEvent.style.display ='block'
-    imgEvent.style.display ='block'
-    imgEventAlt.style.display ='none'
-    imgComm.style.display ='none'
-    imgCommAlt.style.display ='block'
-    imgAudio.style.display ='none'
-    imgAudioAlt.style.display ='block'
-    cardAudio.classList.remove('cardActive')
-    cardEvent.classList.add('cardActive')
-    cardCommu.classList.remove('cardActive')
-}
+// function fEvent(){
+//     divCommu.style.display = 'none'
+//     divAudio.style.display ='none'
+//     divEvent.style.display ='block'
+//     imgEvent.style.display ='block'
+//     imgEventAlt.style.display ='none'
+//     imgComm.style.display ='none'
+//     imgCommAlt.style.display ='block'
+//     imgAudio.style.display ='none'
+//     imgAudioAlt.style.display ='block'
+//     cardAudio.classList.remove('cardActive')
+//     cardEvent.classList.add('cardActive')
+//     cardCommu.classList.remove('cardActive')
+// }
