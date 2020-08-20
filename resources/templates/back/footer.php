a0c8d48e5c25086292bf7d8645b51db65f3b5b95
Abbassihraf <hraf.abbassi@gmail.com>
2020-08-20 18:02:13 +0100

    Create footer.php

---
+++ resources/templates/back/footer.php
@@ -0,0 +1,81 @@
+
+                    <div class="app-wrapper-footer">
+                        <div class="app-footer">
+                            <div class="app-footer__inner">
+                                <div class="app-footer-left">
+                                    <ul class="nav">
+                                        <li class="nav-item">
+                                            <a href="../" class="nav-link">
+                                                Home page
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="javascript:void(0);" class="nav-link">
+                                                Users
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="app-footer-right">
+                                    <ul class="nav">
+                                        <li class="nav-item">
+                                            <a href="logout.php" class="nav-link">
+                                                Logout
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="javascript:void(0);" class="nav-link">
+                                                <div class="badge badge-success mr-1 ml-0">
+                                                    <small>NEW</small>
+                                                </div>
+                                                Learning center
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>    
+                </div>
+                <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+        </div>
+    </div>
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" style="display: none;" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Confirm deletion</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p class="mb-0">Delete this item ?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                <a class="deletion_link" href="">
+                <button type="button" class="btn btn-danger">Delete</button>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script src="//cdn.ckeditor.com/4.14.1/full/ckeditor.js"></script>
+<script>
+CKEDITOR.replace( 'editor' );
+</script>
+
+<script type="text/javascript" src="./assets/scripts/main.js"></script></body>
+    <!-- jquery -->
+
+<script type="text/javascript" src="./assets/scripts/index.js"></script></body>
+
+
+<?php 
+successful_login_notification();
+display_msg();
+ ?>
+</html>
