1f91ed79a0e28001e6e8b950322ab67342e2f0fe
Abbassihraf <hraf.abbassi@gmail.com>
2020-08-20 11:43:01 +0100

    Updating gulp script

--- gulpfile.js
+++ gulpfile.js
@@ -43,6 +43,24 @@ let style = () => {
         // 8. stream changes to all browsers
         .pipe(browserSync.stream());
 }
+let stylePublic = () => {
+    // 1. where is my scss file
+    return gulp.src('src/sass/**/*.scss')
+        // 2. pass the file through sass compiler
+        .pipe(sass().on('error', sass.logError))
+        // 3. pass the file through autoprifixer
+        .pipe(autoprefixer())
+        // 4. pass the file through css minifier
+        .pipe(cssnano())
+        // 5. intilizing sourcemaps
+        .pipe(sourcemaps.init())
+        // 6. pass the files through sourcemaps ?
+        .pipe(sourcemaps.write('.'))
+        // 7. where do i save the compiled CSS ?
+        .pipe(gulp.dest('public/assets/css'))
+        // 8. stream changes to all browsers
+        .pipe(browserSync.stream());
+}
 
 let userref = () => {
     return gulp.src('./src/*.html')
@@ -58,6 +76,15 @@ let userref = () => {
         .pipe(gulp.dest('dist'));
 }
  
+let jspublic = () => {
+    return gulp.src('./src/includes/build_jspublic.html')
+        .pipe(useref())
+        .pipe(gulpIf('*.js', terser()))
+        // 3. pass the file through css minifier
+        // .pipe(gulpIf('*.css', cssnano()))
+
+        .pipe(gulp.dest('public/assets'));
+}
 
 // let fileincludes = () => {
 //     return gulp.src('./*.html')
@@ -89,9 +116,11 @@ function watch() {
     gulp.watch('./src/sass/**/*.scss', userref);
     gulp.watch('./src/*.html', userref);
     gulp.watch('./src/js/**/*.js', userref);
+    gulp.watch('./src/js/**/*.js', jspublic);
     gulp.watch('./src/includes/*.html', userref);
     gulp.watch('./**/*.html').on('change', browserSync.reload);
     gulp.watch('./src/js/**/*.js').on('change', browserSync.reload);
+    gulp.watch('./src/img/', imgminify);
 
 }
 
@@ -99,4 +128,6 @@ exports.style = style;
 exports.watch = watch;
 exports.userref = userref;
 exports.imgminify = imgminify;
+exports.jspublic = jspublic;
+exports.stylePublic = stylePublic;
 // exports.fileincludes = fileincludes;
