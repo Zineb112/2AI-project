5d8a6e9049456ade3a3c5be1a2d056c8f8e9a212
Abbassihraf <hraf.abbassi@gmail.com>
2020-08-20 18:16:24 +0100

    Update index.php

--- public/admin/index.php
+++ public/admin/index.php
@@ -9,7 +9,23 @@ include(TEMPLATE_BACK . DS . "head.php");
 <?php include(TEMPLATE_BACK . DS . "nav.php") ?>
 <?php include(TEMPLATE_BACK . DS . "ui_settings.php") ?>
 
+<div class="app-main">
+<?php include(TEMPLATE_BACK . DS . "sidebar.php") ?>    
+    <div class="app-main__outer">
 
+<?php 
+        $access_admin = 1;
+        $access_editor = 2;
+        $access_viewer = 3;
+        if($_SERVER['REQUEST_URI'] == "/admin/" || $_SERVER['REQUEST_URI'] == "/admin/index.php?loginSuccess" || $_SERVER['REQUEST_URI'] == "/admin/index.php"){
+            include(TEMPLATE_BACK . DS . "dashboard.php") ;
+        }
+
+        // if(isset($_GET['create_partner'])){
+        //     include(TEMPLATE_BACK . DS . "create_partner.php");
+        // }
+
+?>
 
 
 
