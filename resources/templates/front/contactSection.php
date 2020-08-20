8e93fd4985b0369dd722899a2292efed5cc80071
Abbassihraf <hraf.abbassi@gmail.com>
2020-08-20 11:49:49 +0100

    Create contactSection.php

---
+++ resources/templates/front/contactSection.php
@@ -0,0 +1,12 @@
+<section class="contactNous">
+        <div class="contactNous__container">
+            <div class="contactNous__titles">
+                <h3>PRÊT À LE FAIRE</h3>
+                <h4>Mettons-nous au travail!</h4>
+            </div>
+            <div class="contactNous__btns">
+                <a href="contact.html"><button class="contactBtnSection">Contacter nous</button></a>
+                <a href="tel:+212 666666666"><button class="devisBtnSection">obtenir un devis</button></a>
+            </div>
+        </div>
+</section>
