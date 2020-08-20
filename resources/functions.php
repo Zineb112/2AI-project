05c7211d691dd7b0b584d4821024834da9fdb37c
Abbassihraf <hraf.abbassi@gmail.com>
2020-08-20 20:34:32 +0100

    Function to create thumbnails

--- resources/functions.php
+++ resources/functions.php
@@ -89,6 +89,26 @@ function upload_image($name, &$cover_id)
 }
 
 
+/*Function to create thumbnails*/
+function make_thumb($src,$ext , $dest, $desired_width) {
+    /* read the source image */
+    $ext == 'png' ? $source_image = imagecreatefrompng($src) : $source_image = imagecreatefromjpeg($src);
+    // $source_image = imagecreatefromjpeg($src);
+    $width = imagesx($source_image);
+    $height = imagesy($source_image);
+  
+    /* find the “desired height” of this thumbnail, relative to the desired width  */
+    $desired_height = floor($height * ($desired_width / $width));
+  
+    /* create a new, “virtual” image */
+    $virtual_image = imagecreatetruecolor($desired_width, $desired_height);
+  
+    /* copy source image at a resized size */
+    imagecopyresampled($virtual_image, $source_image, 0, 0, 0, 0, $desired_width, $desired_height, $width, $height);
+  
+    /* create the physical thumbnail image to its destination */
+    imagejpeg($virtual_image, $dest);
+}
 
 
 
