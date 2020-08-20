7d87805329cc1ee5f95237f9897ecb9631e52b13
Abbassihraf <hraf.abbassi@gmail.com>
2020-08-20 18:01:36 +0100

    Create index.php

---
+++ public/admin/index.php
@@ -0,0 +1,29 @@
+<?php 
+require_once('../../resources/config.php');
+include("user_auth.php");
+include(TEMPLATE_BACK . DS . "head.php");
+?>
+
+
+
+<?php include(TEMPLATE_BACK . DS . "nav.php") ?>
+<?php include(TEMPLATE_BACK . DS . "ui_settings.php") ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include( TEMPLATE_BACK . DS . "footer.php" )?>
