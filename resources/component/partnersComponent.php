78c11a41a38a6ac4d6f970ea1d160327f5eac06a
Abbassihraf <hraf.abbassi@gmail.com>
2020-08-22 01:35:26 +0100

    Partner Management. display partners to be edited or deleted in admin area

--- resources/component/partnersComponent.php
+++ resources/component/partnersComponent.php
@@ -54,5 +54,38 @@ function submit_partner(){
 }
 
 
+// Partner Management. display partners to be edited or deleted in admin area
+
+function display_partners_admin()
+{
+    global $pdo;
+    try{
+        $sql = "SELECT p.*, m.file_name FROM partners p join media m on p.partner_logo = m.id "; 
+        $stmt = $pdo->query($sql)->fetchAll();
+        foreach ($stmt as $partner){
+        echo <<<partner
+        <tr>
+        <td class="text-center text-muted">{$partner->id}</td>
+        <td class=""><img src="../uploads/thumbnails/{$partner->file_name}" class="br-a" alt="partner thumbnail"></td>
+        <td class=""> {$partner->partner_name} </td>
+        <td class=""> {$partner->partner_link} </td>
+
+        <td class="text-center">
+            <a href="index.php?edit_partner={$partner->id}">
+            <button type="button" id="PopoverCustomT-1"class=" btn-wide btn btn-success btn-icon-only">
+                <i class="pe-7s-note" style="font-size: 1rem;"></i> Edit
+            </button>
+            </a>
+            <button type="button" id="PopoverCustomT-1" class=" btn-icon btn-icon-only btn btn-outline-danger" value="index.php?manage_partner&delete_partner={$partner->id}" data-toggle="modal" data-target="#exampleModal">
+                <i class="pe-7s-trash" style="font-size: 1rem;"></i>
+            </button>
+        </td>
+    </tr>
+partner;
+    }
+} catch (PDOException $e) {
+    echo 'query failed' . $e->getMessage();
+}
+}
 
 ?>
