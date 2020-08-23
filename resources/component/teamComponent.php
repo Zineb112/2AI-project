34262a7dfe1f3a620ef61a9e5e6acf52102e4fff
MinaDakiri <minadakiri7@gmail.com>
2020-08-23 17:16:24 +0100

    Functionality for deleting team members from the db

--- resources/component/teamComponent.php
+++ resources/component/teamComponent.php
@@ -151,15 +151,15 @@ team;
 function delete_team()
 {
     global $pdo;
-    if (isset($_GET['delete_partner'])) {
+    if (isset($_GET['delete_team'])) {
         //Exeption Handling
         try {
             //The SQL statement.
-            $sqlimg = "SELECT m.id, m.file_name FROM partners p join media m on p.partner_logo = m.id WHERE p.id = ?";
+            $sqlimg = "SELECT m.id, m.file_name FROM team t join media m on t.avatar = m.id WHERE t.id = ?";
                 //Prepare our SELECT SQL statement.
                 $stmtimg = $pdo->prepare($sqlimg);
                 //Execute the statement GET the Partner's image data.
-                $stmtimg->execute([$_GET['delete_partner']]);
+                $stmtimg->execute([$_GET['delete_team']]);
                 //fetch the Partner cover data.
                 $img = $stmtimg->fetch();
                 //Check if it's the default image, we don't want to delete the default image.
@@ -169,17 +169,17 @@ function delete_team()
                     //this is not the default image, Now we are going to delete the actual image from the uploads folder.
                     !unlink('../uploads/' . $img->file_name) ? set_message('error', 'cannot delete image due to an error') : set_message('success', 'image has been deleted successfully');
                     //this is not the default image, The query to delete both the image and the partner
-                    $sql = "DELETE p, m FROM partners p join media m on p.partner_logo = m.id WHERE p.id = ?";
+                    $sql = "DELETE t, m FROM team t join media m on t.avatar = m.id WHERE t.id = ?";
                 } else {
                     //this is the default image, The query to delete just the partner
-                    $sql = "DELETE FROM partners WHERE id = ?";
+                    $sql = "DELETE FROM team WHERE id = ?";
                 }
                             //Prepare our DELETE SQL statement.
                 $stmt = $pdo->prepare($sql);
                 //Execute the statement DELETE The partner.
-                $stmt->execute([$_GET['delete_partner']]);
+                $stmt->execute([$_GET['delete_team']]);
                 //display toastr notification, event deleted successfully
-                set_message('success', 'Partner deleted successfully');
+                set_message('success', 'Team deleted successfully');
 }catch (PDOException $e) {
     echo 'query failed' . $e->getMessage();
 }
