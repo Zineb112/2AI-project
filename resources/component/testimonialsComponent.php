7bbc7378e43e5a96ecdbaf58c7d1c2b6aa013166
Salma Belassiri <salmabelassiri@gmail.com>
2020-08-24 13:42:22 +0100

    Add change in estimonialsComponent.php

--- resources/component/testimonialsComponent.php
+++ resources/component/testimonialsComponent.php
@@ -35,7 +35,36 @@ testimonials;
 
 
 
+// submit a testimonials to database admin area
+function submit_testimonials(){
+    global $pdo;
+    if(isset($_POST['submit'])){
+        try{
+        $name = trim($_POST['full_name']);
+        $description = trim($_POST['description']);
+        $role = trim($_POST['role']);
+        $file = $_FILES['profile']['name'];
+        $cover_id = 1;
 
+        //**------  function for handling image upload-------*/
+        upload_image('profile', $cover_id);
+        $sql = "INSERT INTO `testimonials` (`id`, `full_name`, `description`, `role`, `profile`) VALUES (NULL, ?, ?, ?, ?, ?)";
+        $stmt = $pdo->prepare($sql);
+        $result = $stmt->execute([$name, $description, $role, $cover_id]);
+        if($result){
+            set_message('success','testimonials created successfully');
+            
+          } else {
+            set_message('error','try again later');
+            
+          }
+        } catch (PDOException $e) {
+            set_message('error','query failed');
+            
+            echo 'query failed' . $e->getMessage();
+        }
+    }
+}
 
 
 
