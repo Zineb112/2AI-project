20074085b3a8268c8ce1bb08b7cc740feb811d72
Salma Belassiri <salmabelassiri@gmail.com>
2020-08-25 19:22:37 +0100

    Functionality for displaying last carnet from db

--- resources/component/carnetComponent.php
+++ resources/component/carnetComponent.php
@@ -7,13 +7,19 @@ function display_carnet(){
     $sql ="SELECT c.title, c.date, c.file, m.file_name FROM carnet c join media m on c.file = m.id";
     $stmt = $pdo->query($sql)->fetchAll();
     foreach ($stmt as $carnet){
-        echo <<<partners
-        <li class="partners__item">
-        <a href="#" target ="_blank">
-        <img src="uploads/{$partners->file_name}" alt="{$partners->partner_name}" class="partners__img">
-        </a>  
-       </li>
-partners;
+        echo <<<carnet
+
+       <div class="carnet-inv__wrapper">
+       <div class="carnetN__carnet margincarn" data-aos="flip-down" data-aos-duration="1000">
+           <img src="uploads/{$carnet->file_name}" alt="">
+           <div class="carnetN__infos">
+               <h3 class="carnetN__infos--date">{$carnet->date}</h3>
+               <h5 class="carnetN__infos--title"> <span>Titre: </span> {$carnet->title} </h5>
+               <a href="" class="carnetN__download">Télécharger</a>
+           </div>
+       </div>
+       </div>
+carnet;
     }
     } catch (PDOException $e) {
     set_message('error','query failed');
