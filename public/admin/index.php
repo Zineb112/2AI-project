62209c49b181b416754133a5be0233fcd1a73590
MinaDakiri <minadakiri7@gmail.com>
2020-08-25 16:13:24 +0100

    Add if condition for edit_2AINewsC2

--- public/admin/index.php
+++ public/admin/index.php
@@ -48,6 +48,10 @@ include(TEMPLATE_BACK . DS . "head.php");
         if(isset($_GET['manage_2AINewsC2'])){
             include(TEMPLATE_BACK . DS . "2aiNewsC2/manage_2AINewsC2.php");
         }
+        if(isset($_GET['edit_2AINewsC2'])){
+            include(TEMPLATE_BACK . DS . "2aiNewsC2/edit_2AINewsC2.php");
+        }
+       
         // Testimonials requests
         if(isset($_GET['create_testimonials'])){
             include(TEMPLATE_BACK . DS . "testimonials/create_testimonials.php");
