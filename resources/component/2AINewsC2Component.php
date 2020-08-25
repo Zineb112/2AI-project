eceab66d957573fb48499b4c346a88cbe6c0bb7f
MinaDakiri <minadakiri7@gmail.com>
2020-08-25 14:08:21 +0100

    Add formule for manage 2ai News

--- resources/component/2AINewsC2Component.php
+++ resources/component/2AINewsC2Component.php
@@ -57,3 +57,78 @@ function submit_ai_news(){
         }
     }
 }
+//Team Management. display team  to be edited or deleted in admin area
+
+function display_2aiNews_admin()
+{
+    global $pdo;
+    try{
+        $sql = "SELECT a.*, m.file_name FROM ai_news a join media m on a.cover = m.id "; 
+        $stmt = $pdo->query($sql)->fetchAll();
+        foreach ($stmt as $ai_news){
+        echo <<<ainews
+        <tr>
+        <td class="text-center text-muted">{$ai_news->id}</td>
+        <td class=""><img src="../uploads/thumbnails/{$ai_news->file_name}" class="br-a" alt="2ai News thumbnail"></td>
+        <td class=""> {$ai_news->full_name} </td>
+        <td class=""> {$ai_news->link} </td>
+        <td class=""> {$ai_news->role} </td>
+        <td class=""> {$ai_news->title} </td>
+        
+        <td class="text-center">
+            <a href="index.php?edit_2AINewsC2={$ai_news->id}">
+            <button type="button" id="PopoverCustomT-1"class=" btn-wide btn btn-success btn-icon-only">
+                <i class="pe-7s-note" style="font-size: 1rem;"></i> Edit
+            </button>
+            </a>
+            <button type="button" id="PopoverCustomT-1" class=" btn-icon btn-icon-only btn btn-outline-danger" value="index.php?manage_team&delete_team={$ai_news->id}" data-toggle="modal" data-target="#exampleModal">
+                <i class="pe-7s-trash" style="font-size: 1rem;"></i>
+            </button>
+        </td>
+    </tr>
+ainews;
+    }
+} catch (PDOException $e) {
+    echo 'query failed' . $e->getMessage();
+}
+}
+
+
+// Delete a team
+function delete_2ai_news()
+{
+    global $pdo;
+    if (isset($_GET['delete_2ai_news'])) {
+        //Exeption Handling
+        try {
+            //The SQL statement.
+            $sqlimg = "SELECT m.id, m.file_name FROM ai_news a join media m on a.cover = m.id WHERE a.id = ?";
+                //Prepare our SELECT SQL statement.
+                $stmtimg = $pdo->prepare($sqlimg);
+                //Execute the statement GET the Partner's image data.
+                $stmtimg->execute([$_GET['delete_team']]);
+                //fetch the Partner cover data.
+                $img = $stmtimg->fetch();
+                //Check if it's the default image, we don't want to delete the default image.
+                if ($img->id !== '1') {
+                    //this is not the default image, Now we are going to delete thumbnail  from the uploads folder.
+                    !unlink('../uploads/thumbnails/' . $img->file_name) ? set_message('error', 'cannot delete image due to an error') : set_message('success', 'image has been deleted successfully');
+                    //this is not the default image, Now we are going to delete the actual image from the uploads folder.
+                    !unlink('../uploads/' . $img->file_name) ? set_message('error', 'cannot delete image due to an error') : set_message('success', 'image has been deleted successfully');
+                    //this is not the default image, The query to delete both the image and the partner
+                    $sql = "DELETE a, m FROM ai_news a join media m on a.cover = m.id WHERE a.id = ?";
+                } else {
+                    //this is the default image, The query to delete just the partner
+                    $sql = "DELETE FROM ai_news WHERE id = ?";
+                }
+                            //Prepare our DELETE SQL statement.
+                $stmt = $pdo->prepare($sql);
+                //Execute the statement DELETE The partner.
+                $stmt->execute([$_GET['delete_2ai_news']]);
+                //display toastr notification, event deleted successfully
+                set_message('success', '2ai News deleted successfully');
+}catch (PDOException $e) {
+    echo 'query failed' . $e->getMessage();
+}
+}
+}
