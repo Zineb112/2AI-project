1caab880adc3acb594d3a8b0ee3e44ec033700f7
Salma Belassiri <salmabelassiri@gmail.com>
2020-08-26 01:56:19 +0100

    Update manage_carnet.php

---
+++ resources/templates/back/carnet/manage_carnet.php
@@ -0,0 +1,50 @@
+<?php delete_testimonials(); ?>
+<div class="app-main__inner">
+    <div class="app-page-title">
+        <div class="page-title-wrapper">
+            <div class="page-title-heading">
+                <div class="page-title-icon">
+                    <i class="pe-7s-car icon-gradient bg-mean-fruit">
+                    </i>
+                </div>
+                <div>Manage Carnet de l’inventeur
+                    <div class="page-title-subheading">here you can modify your Carnet de l’inventeur
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="main-card mb-3 card">
+                <div class="card-header">Carnet de l’inventeur card</div>
+                <div class="table-responsive">
+                    <table class="align-middle mb-0 table table-borderless table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th class="text-center">#</th>
+                                <th>carnet thumbnail</th>
+                                <th>carnet file</th>
+                                <th>carnet date</th>
+                                <th>carnet title</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php display_carnet_admin() ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    const delete_buttons = document.querySelectorAll('#PopoverCustomT-1');
+    for(const el of delete_buttons ){
+        el.addEventListener('click', (e) => {
+        let link = e.currentTarget.value;
+        document.querySelector('.deletion_link').href = link;
+        });
+    }
+</script>
