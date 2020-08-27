8bfc52b4bff7eed4f1e6f8dd30e413ff0bc07d07
Abbassihraf <hraf.abbassi@gmail.com>
2020-08-27 18:14:52 +0100

    Update portail page

--- public/portailInventeurPage.php
+++ public/portailInventeurPage.php
@@ -13,18 +13,24 @@
                 <img src="images/person1.png" alt="">
                 <a href=""><i class="fas fa-play"></i></a>
             </div>
+            <div class="portailN__inv--bottom">
+                <h3 class="portailN__name">Bill Gates</h3>
+                <h3 class="portailN__role">PDG Microsoft</h3>
+                <h3 class="portailN__titleI">Règles de succès</h3>
+                <a href="" class="portailN__play">Lire la vidéo</a>
+            </div>
+        </div>
+    </div>
+
     <div class="portailIP__pagination">
         <a href="#"><i class="fa fa-angle-left"></i></a>
         <a href="#" class="active">1</a>
         <a href="#">2</a>
         <a href="#">3</a>
         <a href="#"><i class="fa fa-angle-right"></i></a>
-
-        <?php display_portail() ?>
 </div>
 </section>
 </header>
-
 
 <?php include(TEMPLATE_FRONT . DS . "newsletterC2.php") ?>
 <?php include(TEMPLATE_FRONT . DS . "footerC2.php") ?>
