f88a55dd943314f25fa958ae9e29e3c1b3ae1fe8
Zineb112 <57940406+Zineb112@users.noreply.github.com>
2020-08-27 16:45:35 +0100

    portailInventeurPage.php fil

---
+++ public/portailInventeurPage.php
@@ -0,0 +1,94 @@
+@@include('includes/nav.html')
+<header class="hero-headerP">
+@@include('includes/navigationC2.html')
+<section class="portailIP">
+    <h3 class="portailIP__title">
+        Portail De L'inventeur
+    </h3>
+    <div class="portailIP__wrapper">
+        <div class="portailN__inv" data-aos="flip-down" data-aos-duration="1000">
+            <div class="portailN__inv--top">
+                <img src="images/person1.png" alt="">
+                <a href=""><i class="fas fa-play"></i></a>
+            </div>
+            <div class="portailN__inv--bottom">
+                <h3 class="portailN__name">Bill Gates</h3>
+                <h3 class="portailN__role">PDG Microsoft</h3>
+                <h3 class="portailN__titleI">Règles de succès</h3>
+                <a href="" class="portailN__play">Lire la vidéo</a>
+            </div>
+        </div>
+        <div class="portailN__inv" data-aos="flip-down" data-aos-duration="1000">
+            <div class="portailN__inv--top">
+                <img src="images/person2.png" alt="">
+                <a href=""><i class="fas fa-play"></i></a>
+            </div>
+            <div class="portailN__inv--bottom">
+                <h3 class="portailN__name">Bill Gates</h3>
+                <h3 class="portailN__role">PDG Microsoft</h3>
+                <h3 class="portailN__titleI">Règles de succès</h3>
+                <a href="" class="portailN__play">Lire la vidéo</a>
+            </div>
+        </div>
+        <div class="portailN__inv" data-aos="flip-down" data-aos-duration="1000">
+            <div class="portailN__inv--top">
+                <img src="images/person3.png" alt="">
+                <a href=""><i class="fas fa-play"></i></a>
+            </div>
+            <div class="portailN__inv--bottom">
+                <h3 class="portailN__name">Bill Gates</h3>
+                <h3 class="portailN__role">PDG Microsoft</h3>
+                <h3 class="portailN__titleI">Règles de succès</h3>
+                <a href="" class="portailN__play">Lire la vidéo</a>
+            </div>
+        </div>
+        <div class="portailN__inv" data-aos="flip-down" data-aos-duration="1000">
+            <div class="portailN__inv--top">
+                <img src="images/person4.png" alt="">
+                <a href=""><i class="fas fa-play"></i></a>
+            </div>
+            <div class="portailN__inv--bottom">
+                <h3 class="portailN__name">Bill Gates</h3>
+                <h3 class="portailN__role">PDG Microsoft</h3>
+                <h3 class="portailN__titleI">Règles de succès</h3>
+                <a href="" class="portailN__play">Lire la vidéo</a>
+            </div>
+        </div>
+        <div class="portailN__inv" data-aos="flip-down" data-aos-duration="1000">
+            <div class="portailN__inv--top">
+                <img src="images/person5.png" alt="">
+                <a href=""><i class="fas fa-play"></i></a>
+            </div>
+            <div class="portailN__inv--bottom">
+                <h3 class="portailN__name">Bill Gates</h3>
+                <h3 class="portailN__role">PDG Microsoft</h3>
+                <h3 class="portailN__titleI">Règles de succès</h3>
+                <a href="" class="portailN__play">Lire la vidéo</a>
+            </div>
+        </div>
+        <div class="portailN__inv" data-aos="flip-down" data-aos-duration="1000">
+            <div class="portailN__inv--top">
+                <img src="images/person6.png" alt="">
+                <a href=""><i class="fas fa-play"></i></a>
+            </div>
+            <div class="portailN__inv--bottom">
+                <h3 class="portailN__name">Bill Gates</h3>
+                <h3 class="portailN__role">PDG Microsoft</h3>
+                <h3 class="portailN__titleI">Règles de succès</h3>
+                <a href="" class="portailN__play">Lire la vidéo</a>
+            </div>
+        </div>
+    </div>
+    <div class="portailIP__pagination">
+        <a href="#"><i class="fa fa-angle-left"></i></a>
+        <a href="#" class="active">1</a>
+        <a href="#">2</a>
+        <a href="#">3</a>
+        <a href="#"><i class="fa fa-angle-right"></i></a>
+</div>
+</section>
+</header>
+
+@@include('includes/newsletterC2.html')
+@@include('includes/footerC2.html')
+@@include('includes/end.html')
