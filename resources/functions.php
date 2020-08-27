4b69c0e45da10a7865b65cf124053ba500d89f9e
Zineb112 <57940406+Zineb112@users.noreply.github.com>
2020-08-27 17:03:38 +0100

    portailInventeurComponent.php function require

--- resources/functions.php
+++ resources/functions.php
@@ -173,6 +173,7 @@ require_once('component/2AINewsC2Component.php');
 require_once('component/innovComponent.php');
 require_once('component/carnetComponent.php');
 require_once('component/guideComponent.php');
+require_once('component/portailInventeurComponent.php');
 
 
 
