0e693fa769c2d56699d06eddeefc84b54d42ee80
Zineb112 <57940406+Zineb112@users.noreply.github.com>
2020-08-28 14:13:48 +0100

    display_portail function update

--- resources/component/portailInventeurComponent.php
+++ resources/component/portailInventeurComponent.php
@@ -3,27 +3,23 @@
 function display_portail(){
     global $pdo;
     try{
-    $sql ="SELECT p.full_name, p.link, p.cover,p.role, p.title FROM portail p join media m on p.cover = m.id";
+    $sql ="SELECT p.full_name, p.link, p.cover,p.role, p.title, m.file_location FROM portail p join media m on p.cover = m.id";
     $stmt = $pdo->query($sql)->fetchAll();
-    foreach ($stmt as $team){
+    foreach ($stmt as $portail){
         echo <<<portail
-       <div class="team-one__single" data-aos="flip-down" data-aos-duration="1500">
-                    <div class="team-one__image">
-                        <img src="uploads/{$team->file_location}" alt="{$team->full_name}" >
-                        <div class="team-one__social">
-                            <a href="{$team->linkedin}"><i class="fab fa-facebook-f"></i></a>
-                            <a href="{$team->twitter}"><i class="fab fa-twitter"></i></a>
-                            <a href="{$team->gmail}"><i class="fab fa-dribbble"></i></a>
-                            <a href="{$team->instagram}"><i class="fab fa-behance"></i></a>
-                        </div><!-- /.team-one__social -->
-                    </div><!-- /.team-one__image -->
-                    <div class="team-one__content">
-                        <div class="team-one__content-text">
-                            <h3><a href="#">{$team->full_name}</a></h3>
-                            <p>{$team->role}</p>
-                        </div><!-- /.team-one__content-text -->
-                    </div><!-- /.team-one__content -->
-            </div><!-- /.team-one__single -->
+       <div class="portailN__inv" data-aos="flip-down" data-aos-duration="1000">
+            <div class="portailN__inv--top">
+                <img src="uploads/{$portail->location}" alt="{$portail->full_name}">
+                <a href=""><i class="fas fa-play"></i></a>
+            </div>
+            <div class="portailN__inv--bottom">
+                <h3 class="portailN__name">{$portail->full_name}</h3>
+                <h3 class="portailN__role">{$portail->role}</h3>
+                <h3 class="portailN__titleI">{$portail->title}</h3>
+                <a href="{$portail->link}" class="portailN__play">Lire la vidéo</a>
+            </div>
+        </div>
+
 portail;
     }
     } catch (PDOException $e) {
