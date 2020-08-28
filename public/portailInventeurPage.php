4f0d22dec8c34deb975f3e9e168f85f2cbd116b3
Zineb112 <57940406+Zineb112@users.noreply.github.com>
2020-08-28 13:43:16 +0100

    portail updates

--- public/portailInventeurPage.php
+++ public/portailInventeurPage.php
@@ -1,5 +1,3 @@
-<?php require_once('../resources/config.php'); ?>
-
 <?php include(TEMPLATE_FRONT . DS . "nav.php") ?>
 <header class="hero-headerP">
 <?php include(TEMPLATE_FRONT . DS . "navigationC2.php") ?>
@@ -20,8 +18,67 @@
                 <a href="" class="portailN__play">Lire la vidéo</a>
             </div>
         </div>
+        <div class="portailN__inv" data-aos="flip-down" data-aos-duration="1000">
+            <div class="portailN__inv--top">
+                <img src="images/person2.png" alt="">
+                <a href=""><i class="fas fa-play"></i></a>
+            </div>
+            <div class="portailN__inv--bottom">
+                <h3 class="portailN__name">Bill Gates</h3>
+                <h3 class="portailN__role">PDG Microsoft</h3>
+                <h3 class="portailN__titleI">Règles de succès</h3>
+                <a href="" class="portailN__play">Lire la vidéo</a>
+            </div>
+        </div>
+        <div class="portailN__inv" data-aos="flip-down" data-aos-duration="1000">
+            <div class="portailN__inv--top">
+                <img src="images/person3.png" alt="">
+                <a href=""><i class="fas fa-play"></i></a>
+            </div>
+            <div class="portailN__inv--bottom">
+                <h3 class="portailN__name">Bill Gates</h3>
+                <h3 class="portailN__role">PDG Microsoft</h3>
+                <h3 class="portailN__titleI">Règles de succès</h3>
+                <a href="" class="portailN__play">Lire la vidéo</a>
+            </div>
+        </div>
+        <div class="portailN__inv" data-aos="flip-down" data-aos-duration="1000">
+            <div class="portailN__inv--top">
+                <img src="images/person4.png" alt="">
+                <a href=""><i class="fas fa-play"></i></a>
+            </div>
+            <div class="portailN__inv--bottom">
+                <h3 class="portailN__name">Bill Gates</h3>
+                <h3 class="portailN__role">PDG Microsoft</h3>
+                <h3 class="portailN__titleI">Règles de succès</h3>
+                <a href="" class="portailN__play">Lire la vidéo</a>
+            </div>
+        </div>
+        <div class="portailN__inv" data-aos="flip-down" data-aos-duration="1000">
+            <div class="portailN__inv--top">
+                <img src="images/person5.png" alt="">
+                <a href=""><i class="fas fa-play"></i></a>
+            </div>
+            <div class="portailN__inv--bottom">
+                <h3 class="portailN__name">Bill Gates</h3>
+                <h3 class="portailN__role">PDG Microsoft</h3>
+                <h3 class="portailN__titleI">Règles de succès</h3>
+                <a href="" class="portailN__play">Lire la vidéo</a>
+            </div>
+        </div>
+        <div class="portailN__inv" data-aos="flip-down" data-aos-duration="1000">
+            <div class="portailN__inv--top">
+                <img src="images/person6.png" alt="">
+                <a href=""><i class="fas fa-play"></i></a>
+            </div>
+            <div class="portailN__inv--bottom">
+                <h3 class="portailN__name">Bill Gates</h3>
+                <h3 class="portailN__role">PDG Microsoft</h3>
+                <h3 class="portailN__titleI">Règles de succès</h3>
+                <a href="" class="portailN__play">Lire la vidéo</a>
+            </div>
+        </div>
     </div>
-
     <div class="portailIP__pagination">
         <a href="#"><i class="fa fa-angle-left"></i></a>
         <a href="#" class="active">1</a>
--- resources/component/portailInventeurComponent.php
+++ resources/component/portailInventeurComponent.php
@@ -1,27 +1,29 @@
-<?php 
-
+<?php
 
 function display_portail(){
     global $pdo;
     try{
-    $sql ="SELECT p.full_name, p.link, p.cover, p.role, p.title, m.file_name FROM portail p join media m on p.cover = m.id";
+    $sql ="SELECT p.full_name, p.link, p.cover,p.role, p.title FROM portail p join media m on p.cover = m.id";
     $stmt = $pdo->query($sql)->fetchAll();
-    foreach ($stmt as $portail){
+    foreach ($stmt as $team){
         echo <<<portail
-        
-       <div class="portailN__inv--bottom">
-       <h3 class="portailN__name">Bill Gates</h3>
-       <h3 class="portailN__role">PDG Microsoft</h3>
-       <h3 class="portailN__titleI">Règles de succès</h3>
-       <a href="" class="portailN__play">Lire la vidéo</a>
-   </div>
-</div>
-<div class="portailN__inv" data-aos="flip-down" data-aos-duration="1000">
-   <div class="portailN__inv--top">
-       <img src="uploads/{$portail->file_name}" alt="{$portail->full_name}">
-       <a href=""><i class="fas fa-play"></i></a>
-   </div>
-
+       <div class="team-one__single" data-aos="flip-down" data-aos-duration="1500">
+                    <div class="team-one__image">
+                        <img src="uploads/{$team->file_location}" alt="{$team->full_name}" >
+                        <div class="team-one__social">
+                            <a href="{$team->linkedin}"><i class="fab fa-facebook-f"></i></a>
+                            <a href="{$team->twitter}"><i class="fab fa-twitter"></i></a>
+                            <a href="{$team->gmail}"><i class="fab fa-dribbble"></i></a>
+                            <a href="{$team->instagram}"><i class="fab fa-behance"></i></a>
+                        </div><!-- /.team-one__social -->
+                    </div><!-- /.team-one__image -->
+                    <div class="team-one__content">
+                        <div class="team-one__content-text">
+                            <h3><a href="#">{$team->full_name}</a></h3>
+                            <p>{$team->role}</p>
+                        </div><!-- /.team-one__content-text -->
+                    </div><!-- /.team-one__content -->
+            </div><!-- /.team-one__single -->
 portail;
     }
     } catch (PDOException $e) {
@@ -29,7 +31,6 @@ portail;
     echo 'query failed' . $e->getMessage();
     }
 }
-
 
 
 ?>
