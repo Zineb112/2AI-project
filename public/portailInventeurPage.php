19e35454468bcc92872c297ee8403d855b5bd2a5
Zineb112 <57940406+Zineb112@users.noreply.github.com>
2020-08-28 14:13:20 +0100

    portailInenventeurPage.php update

--- public/portailInventeurPage.php
+++ public/portailInventeurPage.php
@@ -1,3 +1,6 @@
+<?php require_once('../resources/config.php'); ?>
+
+
 <?php include(TEMPLATE_FRONT . DS . "nav.php") ?>
 <header class="hero-headerP">
 <?php include(TEMPLATE_FRONT . DS . "navigationC2.php") ?>
@@ -6,86 +9,9 @@
         Portail De L'inventeur
     </h3>
     <div class="portailIP__wrapper">
-        <div class="portailN__inv" data-aos="flip-down" data-aos-duration="1000">
-            <div class="portailN__inv--top">
-                <img src="images/person1.png" alt="">
-                <a href=""><i class="fas fa-play"></i></a>
-            </div>
-            <div class="portailN__inv--bottom">
-                <h3 class="portailN__name">Bill Gates</h3>
-                <h3 class="portailN__role">PDG Microsoft</h3>
-                <h3 class="portailN__titleI">Règles de succès</h3>
-                <a href="" class="portailN__play">Lire la vidéo</a>
-            </div>
-        </div>
-        <div class="portailN__inv" data-aos="flip-down" data-aos-duration="1000">
-            <div class="portailN__inv--top">
-                <img src="images/person2.png" alt="">
-                <a href=""><i class="fas fa-play"></i></a>
-            </div>
-            <div class="portailN__inv--bottom">
-                <h3 class="portailN__name">Bill Gates</h3>
-                <h3 class="portailN__role">PDG Microsoft</h3>
-                <h3 class="portailN__titleI">Règles de succès</h3>
-                <a href="" class="portailN__play">Lire la vidéo</a>
-            </div>
-        </div>
-        <div class="portailN__inv" data-aos="flip-down" data-aos-duration="1000">
-            <div class="portailN__inv--top">
-                <img src="images/person3.png" alt="">
-                <a href=""><i class="fas fa-play"></i></a>
-            </div>
-            <div class="portailN__inv--bottom">
-                <h3 class="portailN__name">Bill Gates</h3>
-                <h3 class="portailN__role">PDG Microsoft</h3>
-                <h3 class="portailN__titleI">Règles de succès</h3>
-                <a href="" class="portailN__play">Lire la vidéo</a>
-            </div>
-        </div>
-        <div class="portailN__inv" data-aos="flip-down" data-aos-duration="1000">
-            <div class="portailN__inv--top">
-                <img src="images/person4.png" alt="">
-                <a href=""><i class="fas fa-play"></i></a>
-            </div>
-            <div class="portailN__inv--bottom">
-                <h3 class="portailN__name">Bill Gates</h3>
-                <h3 class="portailN__role">PDG Microsoft</h3>
-                <h3 class="portailN__titleI">Règles de succès</h3>
-                <a href="" class="portailN__play">Lire la vidéo</a>
-            </div>
-        </div>
-        <div class="portailN__inv" data-aos="flip-down" data-aos-duration="1000">
-            <div class="portailN__inv--top">
-                <img src="images/person5.png" alt="">
-                <a href=""><i class="fas fa-play"></i></a>
-            </div>
-            <div class="portailN__inv--bottom">
-                <h3 class="portailN__name">Bill Gates</h3>
-                <h3 class="portailN__role">PDG Microsoft</h3>
-                <h3 class="portailN__titleI">Règles de succès</h3>
-                <a href="" class="portailN__play">Lire la vidéo</a>
-            </div>
-        </div>
-        <div class="portailN__inv" data-aos="flip-down" data-aos-duration="1000">
-            <div class="portailN__inv--top">
-                <img src="images/person6.png" alt="">
-                <a href=""><i class="fas fa-play"></i></a>
-            </div>
-            <div class="portailN__inv--bottom">
-                <h3 class="portailN__name">Bill Gates</h3>
-                <h3 class="portailN__role">PDG Microsoft</h3>
-                <h3 class="portailN__titleI">Règles de succès</h3>
-                <a href="" class="portailN__play">Lire la vidéo</a>
-            </div>
-        </div>
+    <?php display_portail() ?>
     </div>
-    <div class="portailIP__pagination">
-        <a href="#"><i class="fa fa-angle-left"></i></a>
-        <a href="#" class="active">1</a>
-        <a href="#">2</a>
-        <a href="#">3</a>
-        <a href="#"><i class="fa fa-angle-right"></i></a>
-</div>
+
 </section>
 </header>
 
