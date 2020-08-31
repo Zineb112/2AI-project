92267be0aceac4c223abd2e62aaec55c8905b525
Salma Belassiri <salmabelassiri@gmail.com>
2020-08-31 12:28:46 +0100

    Update manage_newsC2.php

---
+++ resources/templates/back/newsC2/manage_newsC2.php
@@ -0,0 +1,49 @@
+<?php delete_newsC2(); ?>
+<div class="app-main__inner">
+    <div class="app-page-title">
+        <div class="page-title-wrapper">
+            <div class="page-title-heading">
+                <div class="page-title-icon">
+                    <i class="pe-7s-car icon-gradient bg-mean-fruit">
+                    </i>
+                </div>
+                <div>Manage your Blog posts concept 2
+                    <div class="page-title-subheading">Here you can Edit your posts or delete them.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="main-card mb-3 card">
+                <div class="card-header"> Posts list</div>
+                <div class="table-responsive">
+                    <table class="align-middle mb-0 table table-borderless table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th class="text-center">#</th>
+                                <th>Post cover</th>
+                                <th>Title</th>
+                                <th>Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php display_newsC2_admin() ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    const delete_buttons = document.querySelectorAll('#PopoverCustomT-1');
+    for(const el of delete_buttons ){
+        el.addEventListener('click', (e) => {
+        let link = e.currentTarget.value;
+        document.querySelector('.deletion_link').href = link;
+        });
+    }
+</script>
